41cd44306d0f9a35d7e60416e6d84120bac85cdd
Sajjadalgburi <dfgsajjad@gmail.com>
2023-11-27 14:54:34 -0700

    Made generatePassword function

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = prompt(
+    "How many characters would you like your password to be?"
+  );
+  var lowerCase = confirm(
+    "Would you like your password to contain lowercase letters?"
+  );
+  var upperCase = confirm(
+    "Would you like your password to contain uppercase letters?"
+  );
+  var numeric = confirm(
+    "Would you like your password to contain numeric characters?"
+  );
+  var special = confirm(
+    "Would you like your password to contain special characters?"
+  );
+
+  passwordLength = Number(passwordLength);
+
+  if (passwordLength <= 8 || passwordLength >= 128) {
+    window.alert("Password must be between 8 and 128 characters");
+    return;
+  }
+
+  console.log("Testing... Your Pass lenght is " + passwordLength);
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
